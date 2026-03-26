0e989f133071e2bdac5dee7ef4ca15b4ac671857
KavinNishanthan <kavinnishanthan2004@gmail.com>
2026-03-26 18:25:20 +0530

    tablestyle changed in product page

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api', // Backend URL
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: "http://localhost:5001/api", // Backend URL
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // Add a request interceptor to inject the token
